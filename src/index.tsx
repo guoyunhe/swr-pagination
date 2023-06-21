07117472efc1594949ee8bfe87628a1406367d60
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-06-21 17:55:13 +0800

    export mui5 adaptor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export * from './SWRPaginationConfigProvider';
 export * from './api-adaptors/laravel';
 export * from './ui-adaptors/antd5';
+export * from './ui-adaptors/mui5';
 export * from './ui-adaptors/next1';
 export * from './useSWRPagination';
 export * from './useSWRPaginationConfig';
